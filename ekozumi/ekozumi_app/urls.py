81f1ce92bf21c16b5e8760e21d28a4a80801b960
OscarKlemenz <95593957+OscarKlemenz@users.noreply.github.com>
2023-02-14 15:21:03 +0000

    Added login and signup html/css
    HTML/CSS pages for login and sign up have now been implemented, although not functional

--- ekozumi/ekozumi_app/urls.py
+++ ekozumi/ekozumi_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Currently 127.0.0.1:8000/ekozumi/ will go to the login page
 urlpatterns = [
     path("", views.login, name="ekozumi-login"),
+    path("register/", views.register, name="ekozumi-register")
 ]
