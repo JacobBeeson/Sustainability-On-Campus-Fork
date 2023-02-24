5e0f69d96d8d1d403066e9b015fa2148f4632524
Spooks075 <95767695+Spooks075@users.noreply.github.com>
2023-02-24 15:40:05 +0000

    Add files via upload

--- webpages/map.js
+++ webpages/map.js
@@ -20,11 +20,20 @@ function showPosition(position) {
     if (Y_coordinate > 95) {Y_coordinate = 95}
     else if (Y_coordinate < 5) {Y_coordinate = 5}
 
+
     /*changes pet position on map */
     var x = document.getElementById("pet-pos");
     x.style.bottom = Y_coordinate +  "%";
     x.style.left = X_coordinate + "%";
+
+    /*checks if Boss in area */
+    if (X_coordinate>= min_X && X_coordinate <= max_X && Y_coordinate>= min_Y && Y_coordinate<= max_Y){
+        var text = document.getElementById("location");
+        text.innerHTML = "looks like there is something here!";
+    }
     
 }
 
-
+/* duck pond: bottom=20-25% left=10-15% 
+    east park bridge: bottom=55-60% left=65-70%
+    exeter nature reserve: bottom=60-70% left=5-15%*/
