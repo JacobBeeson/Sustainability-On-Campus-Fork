f7ebc31ec219661bae5071c44a3535a8817b0888
Christian Wood <95480214+CW00D@users.noreply.github.com>
2023-03-01 15:27:52 +0000

    Add files via upload

---
+++ ekozumi/ekozumi_app/static/ekozumi_app/zumi_creation.js
@@ -0,0 +1,9 @@
+/** Map logic for map.html
+ * 
+ *  Author: Christian Wood
+ */
+
+function makeFrog(){
+    document.getElementById('petType').value = "Frog";
+    console.log("Turning to frog")
+}
