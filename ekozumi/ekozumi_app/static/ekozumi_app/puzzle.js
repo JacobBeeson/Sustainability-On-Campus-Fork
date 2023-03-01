0751121cc7c0b29b9931fd482e99c7a380dc397b
ElliotWride <93586758+ElliotWride@users.noreply.github.com>
2023-03-01 15:36:16 +0000

    Update puzzle.js

--- ekozumi/ekozumi_app/static/ekozumi_app/puzzle.js
+++ ekozumi/ekozumi_app/static/ekozumi_app/puzzle.js
@@ -14,10 +14,11 @@ function startTime()
     m=checkTime(m);
     s=checkTime(s);
     document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+    //updates clock with current time
     t=setTimeout('startTime()',500);
-    
-    
-    if(true){//change to check if time and if puzzle done
+    //timer until time updates again, 500ms keeps time accurate but doesnt call method too often
+
+    if(true){//change to check if time and if puzzle done - puzzle appears when display is set to block
         document.getElementById('puzzle').style.display = 'block';
     }else{
         document.getElementById('puzzle').style.display = 'none';
@@ -31,17 +32,17 @@ function puzzle(){
     for(var i = 0; i<anagram.length; i++){
         letters[i]=anagram.charAt(i);
     }
-    letters.sort(function() {return 0.5 - Math.random()});
+    letters.sort(function() {return 0.5 - Math.random()});//randomly shuffles array
     
     jumble="";
     for(var i = 0; i<anagram.length; i++){
         jumble += letters[i];
-    }
+    }//coverts to string
     document.getElementById('anagram').innerHTML = jumble;
     startTime();
 }
 
-function checkTime(i)
+function checkTime(i)  //formats single digits
 {
     if (i<10)
         {
@@ -51,10 +52,10 @@ function checkTime(i)
 }
 
 function checkSolve(){
-    answer = (document.f1.nw.value).toUpperCase();
+    answer = (document.f1.nw.value).toUpperCase(); //to ignore case of letters
 
     if (answer == anagram){
         alert("You did it!")
-        document.getElementById('hint').innerHTML="i am the hint";
+        document.getElementById('hint').innerHTML="Get Innovative";
     }
 }
