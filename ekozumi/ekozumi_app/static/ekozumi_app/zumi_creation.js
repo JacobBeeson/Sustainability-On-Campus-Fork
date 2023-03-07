220f50162d28c038ea558ed8cee186e5151cb1b7
Christian Wood <95480214+CW00D@users.noreply.github.com>
2023-03-07 14:58:03 +0000

    Delete ekozumi/ekozumi_app directory

--- ekozumi/ekozumi_app/static/ekozumi_app/zumi_creation.js
+++
@@ -1,9 +0,0 @@
-/** Map logic for map.html
- * 
- *  Author: Christian Wood
- */
-
-function makeFrog(){
-    document.getElementById('petType').value = "Frog";
-    console.log("Turning to frog")
-}
